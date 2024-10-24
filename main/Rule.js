3d875ccf3cc0e4dca127ea58a1fba705309fbea3
togeharuki <23110009@oic-ok.ac.jp>
2024-10-25 01:56:39 +0900

    Rule各種類のファイルを作成/修正

---
+++ main/Rule.js
@@ -0,0 +1,10 @@
+document.getElementById('StartBtn').addEventListener('click', function() {
+    // フェードアウト効果を追加
+    document.body.style.transition = 'opacity 0.5s';
+    document.body.style.opacity = '0';
+    
+    // 0.5秒後に遷移する
+    setTimeout(function() {
+        window.location.href = 'menu.html';
+    }, 500);
+});
