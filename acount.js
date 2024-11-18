89b52d603d568afc65fbcca761705bf91fe41631
kyuna2190 <158013099+kyuna2190@users.noreply.github.com>
2024-11-18 14:55:23 +0900

    写真

--- acount.js
+++ acount.js
@@ -2,52 +2,52 @@
 const DEFAULT_CARDS = [
     {
         name: "逆転の1手",
-        image: "../Deck-Dreamers/kizon/逆転.jpg",
+        image: "https://togeharuki.github.io/Deck-Dreamers/kizon/逆転.jpg",
         effect: "山札から１ドロー"
     },
     {
         name: "手札足りない",
-        image: "../Deck-Dreamers/kizon/手札足りない.jpeg",
+        image: "https://togeharuki.github.io/Deck-Dreamers/kizon/手札足りない.jpeg",
         effect: "山札から１ドロー"
     },
     {
         name: "のぞき見",
-        image: "../Deck-Dreamers/kizon/のぞき見.jpeg",
+        image: "https://togeharuki.github.io/Deck-Dreamers/kizon/のぞき見.jpeg",
         effect: "相手の手札を2枚見る"
     },
     {
         name: "パパラッチ",
-        image: "../Deck-Dreamers/kizon/パパラッチ.jpg",
+        image: "https://togeharuki.github.io/Deck-Dreamers/kizon/パパラッチ.jpg",
         effect: "相手の手札を2枚見る"
     },
     {
         name: "レゴブロック",
-        image: "../Deck-Dreamers/kizon/レゴブロック.jpg",
+        image: "https://togeharuki.github.io/Deck-Dreamers/kizon/レゴブロック.jpg",
         effect: "数値＋２"
     },
     {
         name: "ルブタンの財布",
-        image: "../Deck-Dreamers/kizon/ルブタン.jpg",
+        image: "https://togeharuki.github.io/Deck-Dreamers/kizon/ルブタン.jpg",
         effect: "数値＋２"
     },
     {
         name: "ちくちく",
-        image: "../Deck-Dreamers/kizon/ちくちく.jpg",
+        image: "https://togeharuki.github.io/Deck-Dreamers/kizon/ちくちく.jpg",
         effect: "強制1ダメージ"
     },
     {
         name: "とげとげ",
-        image: "../Deck-Dreamers/kizon/とげとげ.jpg",
+        image: "https://togeharuki.github.io/Deck-Dreamers/kizon/とげとげ.jpg",
         effect: "強制1ダメージ"
     },
     {
         name: "リストカット",
-        image: "../Deck-Dreamers/kizon/りすか.jpg",
+        image: "https://togeharuki.github.io/Deck-Dreamers/kizon/りすか.jpg",
         effect: "両方に2ダメージ"
     },
     {
         name: "共倒れの1手",
-        image: "../Deck-Dreamers/kizon/共倒れ.jpg",
+        image: "https://togeharuki.github.io/Deck-Dreamers/kizon/共倒れ.jpg",
         effect: "両方に2ダメージ"
     }
 ];
