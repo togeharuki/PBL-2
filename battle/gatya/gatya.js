583d620ea471854bde757929d49b03ebe4062d35
haruki1298 <23110009@oic-ok.ac.jp>
2024-11-27 14:32:17 +0900

    ボタン追加

--- battle/gatya/gatya.js
+++ battle/gatya/gatya.js
@@ -1,5 +1,17 @@
-console.log("jsの接続が成功しました");
+document.getElementById('onePull').addEventListener('click', function() {
+    window.location.href = 'https://lucycal.net/web_tool/cpuStressTest/'; // 遷移先のURLを指定
+});
 
-document.getElementById('gachaBox').addEventListener('click', function() {
-    window.location.href = 'https://oic-ok.ac.jp'; // 遷移先のURLを指定
+document.getElementById('onePull').addEventListener('click', function(event) {
+    event.stopPropagation(); // ガチャボックスのクリックイベントを防ぐ
+    console.log("1連ガチャを引きました");
+});
+
+document.getElementById('tenPull').addEventListener('click', function() {
+    window.location.href = 'https://misc.laboradian.com/test/prime-calc/'; // 遷移先のURLを指定
+});
+
+document.getElementById('tenPull').addEventListener('click', function(event) {
+    event.stopPropagation(); // ガチャボックスのクリックイベントを防ぐ
+    console.log("10連ガチャを引きました");
 });
