5f7df393215db9eaea206cf4af0dbce08942360e
kyuna2190 <158013099+kyuna2190@users.noreply.github.com>
2024-12-03 14:07:06 +0900

    modorubotan

--- main/Menu/Menu.js
+++ main/Menu/Menu.js
@@ -24,12 +24,7 @@ document.getElementById('gatyaBth').addEventListener('click', function() {
 document.getElementById('RuleBtn').addEventListener('click', function() {
     navigateTo('../Rule/Rule.html')
 });
-
+// 追加した戻るボタンのイベントリスナー
 document.getElementById('returnButton').addEventListener('click', function() {
-    navigateTo('../../title.html')
-});
-
-// 隠しドットのイベントリスナーを追加
-document.getElementById('secret-dot').addEventListener('click', function() {
-    navigateTo('../../kakusi/kakusi.html');
+    navigateTo('../title.html')
 });
