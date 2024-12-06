1adec63813f7f8aca2e995110c8ea26bb84b998d
haruki1298 <23110009@oic-ok.ac.jp>
2024-12-06 10:33:01 +0900

    Update:ver1.1.2

---
+++ battle/gatya/CardDate.js
@@ -0,0 +1,67 @@
+// ガチャアイテムのデータ
+const GACHA_ITEMS = [
+    {
+        name: '学祭のピザ',
+        image: 'https://togeharuki.github.io/Deck-Dreamers/gatya/写真/R-学祭のピザ.png',
+        effect: '攻撃力+1',
+        count: 10,
+        rarity: 'R',
+        weight: 30
+    },
+    {
+        name: '二郎系',
+        image: 'https://togeharuki.github.io/Deck-Dreamers/gatya/写真/R-二郎系.png',
+        effect: '攻撃力+1',
+        count: 10,
+        rarity: 'R',
+        weight: 30
+    },
+    {
+        name: '先生集合',
+        image: 'https://togeharuki.github.io/Deck-Dreamers/gatya/写真/R-先生集合.png',
+        effect: '攻撃力+1',
+        count: 10,
+        rarity: 'R',
+        weight: 30
+    },
+    {
+        name: '河合家のりょうちゃん',
+        image: 'https://togeharuki.github.io/Deck-Dreamers/gatya/写真/SR-河合家のりょうちゃん.png',
+        effect: '攻撃力+5',
+        count: 5,
+        rarity: 'SR',
+        weight: 10
+    },
+    {
+        name: '金田家のしょうちゃん',
+        image: 'https://togeharuki.github.io/Deck-Dreamers/gatya/写真/SR-金田家のしょうちゃん.png',
+        effect: '攻撃力+3',
+        count: 5,
+        rarity: 'SR',
+        weight: 10
+    },
+    {
+        name: '喜友名家のともちゃん',
+        image: 'https://togeharuki.github.io/Deck-Dreamers/gatya/写真/SR-喜友名家のともちゃん.png',
+        effect: '攻撃力+2',
+        count: 5,
+        rarity: 'SR',
+        weight: 10
+    },
+    {
+        name: '佐藤家のてんちゃん',
+        image: 'https://togeharuki.github.io/Deck-Dreamers/gatya/写真/SSR-佐藤家のてんちゃん.png',
+        effect: '回復+10',
+        count: 2,
+        rarity: 'SSR',
+        weight: 2
+    },
+    {
+        name: 'マーモット系男子',
+        image: 'https://togeharuki.github.io/Deck-Dreamers/gatya/写真/SSR-マーモット系男子.png',
+        effect: '回復+10',
+        count: 2,
+        rarity: 'SSR',
+        weight: 2
+    }
+];
