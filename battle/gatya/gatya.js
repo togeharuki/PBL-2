070e804dd2af3e4a4036150a605bb4049aba1def
haruki1298 <23110009@oic-ok.ac.jp>
2024-12-13 20:17:07 +0900

    firebaseの保存方法決定

--- battle/gatya/gatya.js
+++ battle/gatya/gatya.js
@@ -164,7 +164,7 @@ async function addCardToSouko(card) {
         const cards = Object.values(existingData).filter(item => item.type === 'gacha');
         
         // 新しいカードIDを生成
-        const newCardId = `default_card_0${Date.now()}_${Math.random().toString(36).substr(2, 9)}_gacha`;
+        const newCardId = `default_card_0${Date.now()}_${Math.random().toString(5)}`;
         
         // 新しいカードデータを作成
         const newData = {
